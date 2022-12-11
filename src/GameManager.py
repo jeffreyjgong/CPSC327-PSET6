04cbca4342836d7fe3aaabd7d662ff828c971212
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-12-11 15:13:57 -0500

    Add initial structure

---
+++ src/GameManager.py
@@ -0,0 +1,21 @@
+class GameManager:
+   """
+   A class to represent the Game CLI interface
+
+   Attributes
+   ----------
+   TBD
+   """
+   def __init__(self):
+      """
+      Constructs the necessary attributes for the BankCLI object
+      """
+   
+   def run():
+      """
+      Executes the game loop
+      """
+
+if __name__ == '__main__':
+   SantoriniCLI = GameManager()
+   SantoriniCLI.run()
