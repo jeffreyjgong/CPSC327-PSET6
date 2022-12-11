e333ae61a6ef2afe280f8254e70636c5dbbd5b12
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-12-11 17:38:08 -0500

    Snake casing

--- src/GameManager.py
+++ src/GameManager.py
@@ -21,14 +21,14 @@ class GameManager:
       
 
 if __name__ == '__main__':
-   allowedParams = [['human', 'heuristic', 'random'], ['human', 'heuristic', 'random'], ['on', 'off'], ['on', 'off']]
+   allowed_params = [['human', 'heuristic', 'random'], ['human', 'heuristic', 'random'], ['on', 'off'], ['on', 'off']]
 
    # initially loaded with default values
    params = ['human', 'human', 'off', 'off']
 
    for i in range(1, len(sys.argv)):
       # check if valid
-      if (sys.argv[i] not in allowedParams[i-1]):
+      if (sys.argv[i] not in allowed_params[i-1]):
          sys.exit("Invalid command line argument: " + sys.argv[i])
       
       # set param
