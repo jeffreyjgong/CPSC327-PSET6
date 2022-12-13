59e14eb310a5fe7eb7aa8261adb3d15351219657
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-12-13 17:44:08 -0500

    initial heuristic structure

--- src/Game/Player/HeuristicPlayer.py
+++ src/Game/Player/HeuristicPlayer.py
@@ -6,13 +6,24 @@ class HeuristicPlayer(Player):
    """
 
    def __init__(self, player_id, board):
+      self._height_weight = 3
+      self._center_weight = 2
+      self._distance_weight = 1
+      self._worker_choice = ''
+      self._move_choice = ''
       super().__init__(player_id, board)
    
    def select_worker(self):
-      return super().select_worker()
+      worker_choice = ''
+      self._worker_choice = worker_choice
+      return worker_choice
 
    def select_direction(self):
-      return super().select_direction()
+      move_choice = ''
+      self._move_choice = move_choice
+      return move_choice
 
    def select_build_direction(self):
-      return super().select_build_direction()
+      build_choice = ''
+      print(self._worker_choice + ',' + self._move_choice + ',' + build_choice)
+      return build_choice
